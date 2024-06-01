c3a3058177873a2cee46db4aec49edf7cb4d5438
iferdel <ignaferdel@gmail.com>
2024-06-01 10:11:03 -0400

    dynamic printing of commands in help callback

--- command_help.go
+++ command_help.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func commandHelp() error {
-	fmt.Println("this is the help menu for the Chile Economic Indexes CLI tool")
+	fmt.Println("This is the help menu for the Chile Economic Indexes CLI tool")
+	fmt.Println("All available commands are listed below:")
+
+	availableCommands := getCommands()
+	for _, command := range availableCommands {
+		fmt.Printf(" - %s: %s\n", command.name, command.description)
+	}
+	fmt.Println("")
+
 	return nil
 }
