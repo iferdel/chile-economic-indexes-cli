81d3566f8f6be6cfd4b54c37e7e9a7907791426d
iferdel <ignaferdel@gmail.com>
2024-06-02 13:43:14 -0400

    save and load credentials locally

--- command_help.go
+++ command_help.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func commandHelp(cfg *config, args ...string) error {
+	fmt.Printf("Connected as %q\n", cfg.bcchapiClient.AuthConfig.User)
 	fmt.Println("This is the help menu for the Chile Economic Indexes CLI tool")
 	fmt.Println("All available commands are listed below:")
 
--- command_login.go
+++ command_login.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
 func commandLogin(cfg *config, args ...string) error {
 
+	defer saveLocalCredentials(cfg, bcchCredentials)
 	if len(args) != 2 {
 		return fmt.Errorf("usage: login <user> <password>")
 	}
@@ -13,4 +18,32 @@ func commandLogin(cfg *config, args ...string) error {
 
 	return nil
 
+}
+
+func loadLocalCredentials(cfg *config, filename string) error {
+	dat, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(dat, &cfg.bcchapiClient.AuthConfig)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func saveLocalCredentials(cfg *config, filename string) error {
+	data, err := json.MarshalIndent(cfg.bcchapiClient.AuthConfig, "", "  ")
+	if err != nil {
+		return err
+	}
+	if f, err := os.Create(filename); err == nil {
+		defer f.Close()
+		_, err := f.Write(data)
+		if err != nil {
+			return fmt.Errorf("error saving credentials before exiting CLI")
+		}
+	}
+	return nil
 }
