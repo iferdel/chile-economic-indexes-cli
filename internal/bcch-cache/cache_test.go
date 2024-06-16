f505cdfc29a4f8b8a2e08475a391c1189ce1ea80
iferdel <ignaferdel@gmail.com>
2024-06-16 11:46:21 -0400

    get and add methods for Cache on  tests

--- internal/bcch-cache/cache_test.go
+++ internal/bcch-cache/cache_test.go
@@ -1,6 +1,7 @@
 package bcchcache
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
@@ -10,5 +11,52 @@ func TestCreateCache(t *testing.T) {
 	cache := NewCache(interval)
 	if cache.cache == nil {
 		t.Error("shouldn't return nil cache map")
+	}
+}
+
+func TestAddGet(t *testing.T) {
+	const interval = 5 * time.Second
+	cases := []struct {
+		key string //endpoint
+		val []byte //content
+	}{
+		{
+			key: "https://example.com",
+			val: []byte("test-data"),
+		},
+		{
+			key: "",
+			val: []byte(""),
+		},
+	}
+
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("test case %v", i), func(t *testing.T) {
+
+			cache := NewCache(interval)
+
+			err := cache.Add(c.key, c.val)
+			if c.key == "" {
+				if err == nil {
+					t.Errorf("expected error caused by empty key to be added in cache")
+				}
+				return
+			}
+			if err != nil {
+				t.Errorf("unexpected error adding key %v in test case %v: %v", c.val, i, err)
+				return
+			}
+
+			got, ok := cache.Get(c.key)
+			if !ok {
+				t.Errorf("expected key %v to be found in test case %v", c.key, i)
+				return
+			}
+
+			if string(got) != string(c.val) {
+				t.Errorf("expected value %v, but got %v value", c.val, got)
+			}
+
+		})
 	}
 }
