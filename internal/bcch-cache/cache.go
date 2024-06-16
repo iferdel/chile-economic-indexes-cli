e5cea16287f2b55afcbfbc633c8da9fd661dcf2c
iferdel <ignaferdel@gmail.com>
2024-06-16 11:52:04 -0400

    cache add and get methods

--- internal/bcch-cache/cache.go
+++ internal/bcch-cache/cache.go
@@ -1,6 +1,9 @@
 package bcchcache
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Cache struct {
 	cache map[string]cacheEntry
@@ -16,4 +19,22 @@ func NewCache(interval time.Duration) Cache {
 		cache: make(map[string]cacheEntry),
 	}
 	return c
+}
+
+func (c *Cache) Add(key string, value []byte) error {
+	if key == "" {
+		return fmt.Errorf("cannot add empty key")
+	}
+
+	c.cache[key] = cacheEntry{
+		createdAt: time.Now(),
+		value:     value,
+	}
+
+	return nil
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	entry, ok := c.cache[key]
+	return entry.value, ok
 }
