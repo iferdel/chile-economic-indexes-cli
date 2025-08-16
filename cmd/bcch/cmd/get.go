3b64eb78e23013f8fc914f6bddeecdbc5a3bb986
iferdel <ignaferdel@gmail.com>
2025-08-16 13:33:25 -0400

    cli now in cmd bcch folder

---
+++ cmd/bcch/cmd/get.go
@@ -0,0 +1,60 @@
+package cmd
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+const dateLayout = "2006-01-02"
+
+var getCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Retrieve data from specific series ID",
+	Run: withSpinnerWrapper(cfg.spinner, func(cmd *cobra.Command, args []string) {
+		err := loadLocalCredentials(&cfg, bcchCredentials)
+		if err != nil {
+			fmt.Println(err)
+		}
+		creds := cfg.bcchapiClient.AuthConfig
+		if creds.User == "" || creds.Password == "" {
+			fmt.Println("you need to first set your BCCH credentials to use this command, see 'help' for details")
+		}
+
+		seriesFlag, _ := cmd.Flags().GetString("series")
+		firstDateFlag, _ := cmd.Flags().GetString("firstdate")
+		lastDateFlag, _ := cmd.Flags().GetString("lastdate")
+
+		if firstDateFlag != "" {
+			if _, err := time.Parse(dateLayout, firstDateFlag); err != nil {
+				fmt.Printf("Invalid firstdate '%s': must be YYYY-MM-DD\n", firstDateFlag)
+				return
+			}
+		}
+		if lastDateFlag != "" {
+			if _, err := time.Parse(dateLayout, lastDateFlag); err != nil {
+				fmt.Printf("Invalid lastdate '%s': must be YYYY-MM-DD\n", lastDateFlag)
+				return
+			}
+		}
+
+		seriesData, _ := cfg.bcchapiClient.GetSeriesData(seriesFlag, firstDateFlag, lastDateFlag)
+
+		if seriesData.Codigo != 0 {
+			fmt.Println(seriesData.Descripcion)
+		}
+		// placeholder for spinner last symbol
+		fmt.Println("")
+		for _, series := range seriesData.Series.Obs {
+			fmt.Printf("%v - %v\n", series.IndexDateString, series.Value)
+		}
+	}),
+}
+
+func init() {
+	rootCmd.AddCommand(getCmd)
+	getCmd.Flags().StringP("series", "s", "", "series ID")
+	getCmd.Flags().String("firstdate", "", "first date in YYYY-MM-DD format (optional)")
+	getCmd.Flags().String("lastdate", "", "last date in YYYY-MM-DD format (optional)")
+}
---
+++ cmd/bcch/cmd/setcredentials.go
@@ -0,0 +1,63 @@
+package cmd
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+)
+
+// BCCH does not have any login feature, but the url for api requests uses the credentials.
+var setCredentialsCmd = &cobra.Command{
+	Use:   "setCredentials",
+	Short: "set credentials to be used to retrieve data from BCCh API",
+	Long: `It saves the credentials in a local file.
+    The local file is generated in the same folder where the cmd is used`,
+	Run: func(cmd *cobra.Command, args []string) {
+		userFlag, _ := cmd.Flags().GetString("user")
+		passwordFlag, _ := cmd.Flags().GetString("password")
+		cfg.bcchapiClient.AuthConfig.User = userFlag
+		cfg.bcchapiClient.AuthConfig.Password = passwordFlag
+		saveLocalCredentials(cfg, bcchCredentials) // #nosec G104
+		fmt.Println("saved credentials!")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(setCredentialsCmd)
+	setCredentialsCmd.Flags().StringP("user", "u", "", "user for for bcch")
+	setCredentialsCmd.Flags().StringP("password", "p", "", "password for bcch")
+	setCredentialsCmd.MarkFlagsRequiredTogether("user", "password")
+}
+
+func loadLocalCredentials(cfg *config, filename string) error {
+	dat, err := os.ReadFile(filepath.Clean(filename))
+	if err != nil {
+		return errors.New("no credentials yet saved, 'set-credentials' saves credentials for future sessions")
+	}
+
+	err = json.Unmarshal(dat, &cfg.bcchapiClient.AuthConfig)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func saveLocalCredentials(cfg config, filename string) error {
+	data, err := json.MarshalIndent(cfg.bcchapiClient.AuthConfig, "", "  ")
+	if err != nil {
+		return err
+	}
+	if f, err := os.Create(filepath.Clean(filename)); err == nil {
+		defer f.Close()
+		_, err := f.Write(data)
+		if err != nil {
+			return fmt.Errorf("error saving credentials before exiting CLI")
+		}
+	}
+	return nil
+}
